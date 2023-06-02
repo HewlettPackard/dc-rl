717cd9efa6a0d52a6d13cc9de1c1aae9f515f6f3
Dheeraj Kashyap <lekhapriya.dheeraj-kashyap@hpe.com>
2023-06-02 14:04:20 -0700

    adding theme

--- sphinx/_build/html/_sources/index.rst.txt
+++ sphinx/_build/html/_sources/index.rst.txt
@@ -6,6 +6,15 @@
 DCRL-Green
 ==========
 
+DCRL-green is a multi-agent Reinforcement Learning (MARL) framework that optimizes data centers for multiple objectives of carbon footprint reduction, energy consumption, and energy
+cost. 
+
+.. image:: images/System-2.png
+   :scale: 10 %
+   :alt: Overview of the physical and digital systems
+   :align: center
+
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
--- sphinx/conf.py
+++ sphinx/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,10 +31,22 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.bibtex', # for citations
+    'sphinxemoji.sphinxemoji', # for emojis
+    'sphinx_copybutton', # to copy code block
+    'sphinx_panels', # for backgrounds
 ]
+
+# source for bib references
+bibtex_bibfiles = ['references.bib']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+source_suffix = '.rst'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,7 +59,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
