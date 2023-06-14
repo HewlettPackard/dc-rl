357a44c7c33b2b86afc76e77d7b24ea97be79b94
Antonio Guillen <guillant@ai07.labs.hpecorp.net>
2023-06-14 14:29:32 -0700

    added more reward functions

--- utils/reward_creator.py
+++ utils/reward_creator.py
@@ -1,20 +1,65 @@
 def default_ls_reward(params: dict) -> float:
+    """
+    Calculates a reward value based on normalized load shifting.
+
+    Args:
+        params (dict): Dictionary containing parameters:
+            norm_load_left (float): Normalized load left.
+            out_of_time (bool): Indicator whether the agent is out of time.
+            penalty (float): Penalty value.
+
+    Returns:
+        float: Reward value.
+    """
     norm_load_left = params['norm_load_left']
     out_of_time = params['out_of_time']
     penalty = params['penalty']
+    
     if out_of_time:
         reward = -norm_load_left*penalty
     else:
         reward = 0
     return reward
 
+
 def default_dc_reward(params: dict) -> float:
+    """
+    Calculates a reward value based on the data center's total ITE Load and CT Cooling load.
+
+    Args:
+        params (dict): Dictionary containing parameters:
+            data_center_total_ITE_Load (float): Total ITE Load of the data center.
+            CT_Cooling_load (float): CT Cooling load of the data center.
+            energy_lb (float): Lower bound of the energy.
+            energy_ub (float): Upper bound of the energy.
+
+    Returns:
+        float: Reward value.
+    """
     data_center_total_ITE_Load = params['data_center_total_ITE_Load']
     CT_Cooling_load = params['CT_Cooling_load']
     energy_lb,  energy_ub = params['energy_lb'], params['energy_ub']
+    
     return - 1.0 * ((data_center_total_ITE_Load + CT_Cooling_load)-energy_lb)/(energy_ub-energy_lb)
 
+
 def default_bat_reward(params: dict) -> float:
+    """
+    Calculates a reward value based on the battery usage.
+
+    Args:
+        params (dict): Dictionary containing parameters:
+            total_energy_with_battery (float): Total energy with battery.
+            norm_CI (float): Normalized Carbon Intensity.
+            a_t (float): Action at time t.
+            dcload_min (float): Minimum DC load.
+            dcload_max (float): Maximum DC load.
+            battery_current_load (float): Current load of the battery.
+            max_bat_cap (float): Maximum battery capacity.
+
+    Returns:
+        float: Reward value.
+    """
     total_energy_with_battery = params['total_energy_with_battery']
     norm_CI = params['norm_CI']
     a_t = params['a_t']
@@ -28,6 +73,7 @@ def default_bat_reward(params: dict) -> float:
 
     return rew_footprint
 
+
 # Example custom reward method
 def custom_agent_reward(params: dict) -> float:
     #read reward input parameters from dict object
@@ -35,12 +81,152 @@ def custom_agent_reward(params: dict) -> float:
     custom_reward = 0.0 #update with custom reward shaping 
     return custom_reward
 
+
+# Example of ToU reward based on energy usage and price of electricity
+# ToU reward is based on the ToU (Time of Use) of the agent, which is the amount of the energy time
+# the agent spends on the grid times the price of the electricity.
+# This example suppose that inside the params there are the following keys:
+#   - 'energy_usage': the energy usage of the agent
+#   - 'hour': the hour of the day
+def tou_reward(params: dict) -> float:
+    # ToU dict: {Hour, price}
+    tou = {0: 0.25,
+           1: 0.25,
+           2: 0.25,
+           3: 0.25,
+           4: 0.25,
+           5: 0.25,
+           6: 0.41,
+           7: 0.41,
+           8: 0.41,
+           9: 0.41,
+           10: 0.41,
+           11: 0.30,
+           12: 0.30,
+           13: 0.30,
+           14: 0.30,
+           15: 0.30,
+           16: 0.27,
+           17: 0.27,
+           18: 0.27,
+           19: 0.27,
+           20: 0.27,
+           21: 0.27,
+           22: 0.25,
+           23: 0.25,
+           }
+    
+    current_price = tou[params['hour']]
+    energy_usage = params['energy_usage']
+    
+    tou_reward = -1.0 * energy_usage * current_price
+
+    return tou_reward
+
+
+def renewable_energy_reward(params: dict) -> float:
+    """
+    Calculates a reward value based on the usage of renewable energy sources.
+
+    Args:
+        params (dict): Dictionary containing parameters:
+            renewable_energy_ratio (float): Ratio of energy coming from renewable sources.
+            total_energy_consumption (float): Total energy consumption of the data center.
+
+    Returns:
+        float: Reward value.
+    """
+    renewable_energy_ratio = params['renewable_energy_ratio']
+    total_energy_consumption = params['total_energy_consumption']
+    max_possible_reward = 1.0  # decide how much weight you want to give to renewable energy usage. 
+
+    reward = max_possible_reward * renewable_energy_ratio - total_energy_consumption
+    return reward
+
+
+def energy_efficiency_reward(params: dict) -> float:
+    """
+    Calculates a reward value based on energy efficiency.
+
+    Args:
+        params (dict): Dictionary containing parameters:
+            ITE_load (float): The amount of energy spent on computation (useful work).
+            total_energy_consumption (float): Total energy consumption of the data center.
+
+    Returns:
+        float: Reward value.
+    """
+    ITE_load = params['ITE_load']  
+    total_energy_consumption = params['total_energy_consumption']  
+    
+    reward = ITE_load / total_energy_consumption
+    return reward
+
+
+def energy_PUE_reward(params: dict) -> float:
+    """
+    Calculates a reward value based on Power Usage Effectiveness (PUE).
+
+    Args:
+        params (dict): Dictionary containing parameters:
+            total_energy_consumption (float): Total energy consumption of the data center.
+            it_equipment_energy (float): Energy consumed by the IT equipment.
+
+    Returns:
+        float: Reward value.
+    """
+    total_energy_consumption = params['total_energy_consumption']  
+    it_equipment_energy = params['it_equipment_energy']  
+    
+    # Calculate PUE
+    pue = total_energy_consumption / it_equipment_energy if it_equipment_energy != 0 else float('inf')
+    
+    # We aim to get PUE as close to 1 as possible, hence we take the absolute difference between PUE and 1
+    # We use a negative sign since RL seeks to maximize reward, but we want to minimize PUE
+    reward = -abs(pue - 1)
+    
+    return reward
+
+
+def temperature_efficiency_reward(params: dict) -> float:
+    """
+    Calculates a reward value based on the efficiency of cooling in the data center.
+
+    Args:
+        params (dict): Dictionary containing parameters:
+            current_temperature (float): Current temperature in the data center.
+            optimal_temperature_range (tuple): Tuple containing the minimum and maximum optimal temperatures for the data center.
+
+    Returns:
+        float: Reward value.
+    """
+    current_temperature = params['current_temperature'] 
+    optimal_temperature_range = params['optimal_temperature_range']
+    min_temp, max_temp = optimal_temperature_range
+    
+    if min_temp <= current_temperature <= max_temp:
+        reward = 1.0
+    else:
+        if current_temperature < min_temp:
+            reward = -abs(current_temperature - min_temp)
+        else:
+            reward = -abs(current_temperature - max_temp)
+    return reward
+
+
+# Other reward methods can be added here.
+
 REWARD_METHOD_MAP = {
     'default_dc_reward' : default_dc_reward,
     'default_bat_reward': default_bat_reward,
     'default_ls_reward' : default_ls_reward,
     # Add custom reward methods here
     'custom_agent_reward' : custom_agent_reward,
+    'tou_reward' : tou_reward,
+    'renewable_energy_reward': renewable_energy_reward,
+    'energy_efficiency_reward': energy_efficiency_reward,
+    'energy_PUE_reward': energy_PUE_reward,
+    'temperature_efficiency_reward': temperature_efficiency_reward,
 }
 
 def get_reward_method(reward_method : str = 'default_dc_reward'):
