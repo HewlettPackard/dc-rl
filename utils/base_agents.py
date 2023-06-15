bd681c691a72daf31c1627ac98fae33fb18e1b27
Antonio Guillen <guillant@ai07.labs.hpecorp.net>
2023-06-15 16:42:45 -0700

    generating docstring

--- utils/base_agents.py
+++ utils/base_agents.py
@@ -1,19 +1,56 @@
 import numpy as np
 
 class BaseLoadShiftingAgent:
+    """
+    Base class for load shifting agents.
+    """
     def __init__(self, parameters=None):
+        """
+        Args:
+            parameters (dict) :  Dictionary containing the agent parameters.
+        """
         self.parameters = parameters
     def do_nothing_action(self):
+        """
+        Return the do nothing action.
+        
+            action (int): The action (do nothing) to be taken.
+        """
         return 1
     
 class BaseHVACAgent:
+    """
+    Base class for HVAC agents.
+    """
     def __init__(self, parameters=None):
+        """
+        Args:
+            parameters (dict) :  Dictionary containing the agent parameters.
+        """
         self.parameters = parameters
     def do_nothing_action(self):
+        """
+        Return the do nothing action.
+        
+            action (int): The action (do nothing) to be taken.
+        """
         return np.int64(4)
 
 class BaseBatteryAgent:
+    """
+    Base class for battery agents.
+    """
     def __init__(self, parameters=None):
+        """
+        Args:
+            parameters (dict) :  Dictionary containing the agent parameters.
+        """
         self.parameters = parameters
     def do_nothing_action(self):
+        """
+        Return the do nothing action.
+        
+        Returns:
+            action (int): The action (do nothing) to be taken.
+        """
         return 2
