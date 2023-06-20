9d52c5ebcea79b8eca2fd52c9a2a990d742ec9b3
Antonio Guillen <antonio.guillen@hpe.com>
2023-06-20 14:23:20 -0700

    added __init__.py s

--- train_a2c.py
+++ train_a2c.py
@@ -46,10 +46,12 @@ CONFIG = (
         .framework("torch")
         .rollouts(num_rollout_workers=NUM_WORKERS)
         .training(
+            use_gae=True,
             gamma=0.99, 
-            lr=5e-6,
+            lr=1e-4,
             train_batch_size=24 * TIMESTEP_PER_HOUR * COLLECTED_DAYS * NUM_WORKERS * NUM_AGENTS,
             model={'fcnet_hiddens': [128, 64, 16], 'fcnet_activation': 'relu'}, 
+            lr_schedule=[[0, 1e-3], [1000000, 1e-5]],
         )
         .callbacks(CustomCallbacks)
         .resources(num_cpus_per_worker=2, num_gpus=0)
