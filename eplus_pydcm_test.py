cac1e67ae9c2b9f032e5fc31ec911fa41756b04f
Avisek Naug <naug@ai08.labs.hpecorp.net>
2023-09-19 14:20:29 -0700

    create time compare script

--- eplus_pydcm_test.py
+++ eplus_pydcm_test.py
@@ -13,16 +13,16 @@ ci_loc, wea_loc = obtain_paths(location)
 month = 0
 
 
-dcrl_dc_env = make_dc_pyeplus_env(month+1, ci_loc, max_bat_cap_Mw=2.0, use_ls_cpu_load=True) 
+pydcm_env = make_dc_pyeplus_env(month+1, ci_loc, max_bat_cap_Mw=2.0, use_ls_cpu_load=True) 
 eplus_dc_env = make_dc_env(month, location) 
 
 # Measure the step time
 action = np.int64(4)
 num_steps = 1000
-_ = dcrl_dc_env.reset()
+_ = pydcm_env.reset()
 start_time = time.time()
 for _ in range(0, num_steps):
-    dcrl_dc_env.step(action)
+    pydcm_env.step(action)
 avg_step_time_dcrl = (time.time() - start_time) / num_steps
 print("######################################################")
 print(f'PyDCM avg step time: {avg_step_time_dcrl:.5f} seconds')
@@ -57,5 +57,5 @@ print("######################################################")
 # Create a tqdm progress bar to wrap your loop and enable the timer
 with tqdm(total=1000, desc="Processing", unit="iteration") as pbar:
     for _ in range(1000):
-        dc_state, _, dc_terminated, dc_truncated, dc_info = dcrl_dc_env.step(action)
+        dc_state, _, dc_terminated, dc_truncated, dc_info = pydcm_env.step(action)
         pbar.update(1)
