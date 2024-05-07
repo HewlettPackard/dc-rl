f790d10f6b7e6012a04d1bca5b8c245efe62aad1
Avisek Naug <avisek.naug@hpe.com>
2024-05-07 19:38:35 +0000

    set up methods

--- geo_dcrl.py
+++ geo_dcrl.py
@@ -10,10 +10,10 @@ import numpy as np
 from tqdm import tqdm
 import gymnasium as gym
 from gymnasium.spaces import Dict, Box, MultiDiscrete, Tuple, Discrete
-
+from hierarchical_workload_optimizer import WorkloadOptimizer
 from dcrl_env_harl_partialobs import DCRL as DCRLPartObs
 
-sys.path.insert(0, f'{os.path.dirname(os.path.abspath(__file__))}/HARL')
+sys.path.insert(0, f'{os.path.dirname(os.path.abspath(__file__))}/heterogeneous_dcrl')
 from harl.runners import RUNNER_REGISTRY
 from harl.utils.configs_tools import get_defaults_yaml_args
 from utils.base_agents import *
@@ -180,10 +180,130 @@ class HARL_HeirarchicalDCRL(gym.Env):
             "receiver": Discrete(3),
             "workload_to_move": Box(0., 1., [1])
         })
+        
+    def reset(self, seed=None, options=None):
+
+        # Set seed if we are not in rllib
+        if seed is not None:
+            seed = 12
+            np.random.seed(seed)
+            random.seed(seed)
+            torch.manual_seed(seed)
+
+        self.low_level_observations = {}
+        self.heir_obs = {}
+        infos = {}
+
+        # Reset environments and store initial observations and infos
+        for env_id, env in self.datacenters.items():
+            obs, info, available_actions = env.reset()
+            self.low_level_observations[env_id] = obs
+            self.low_level_infos[env_id] = info
+            
+            self.heir_obs[env_id] = self.get_dc_variables(env_id)
+            
+        # Initialize metrics
+        self.metrics = {
+            env_id: {
+                'bat_CO2_footprint': [],
+                'bat_total_energy_with_battery_KWh': [],
+                'ls_tasks_dropped': [],
+                'dc_water_usage': [],
+            }
+            for env_id in self.datacenters
+        }
+
+        self.all_done = {env_id: False for env_id in self.datacenters}
+        
+        return self.heir_obs, self.low_level_infos
+    
+    def step(self, action: Any) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
+        return super().step(action)
+    
+    def set_hierarchical_workload(self, dc_id: str, workload: float):
+        pass
+    
+    def compute_adjusted_workloads(self, actions) -> dict:
+        pass
+    
+    def set_hysterisis(self, mwh_to_move: float, sender: str, receiver: str):
+        pass
+    
+    def calc_reward(self):
+        pass
+    
+    def get_dc_variables(self, dc_id: str):
+        
+        dc = self.datacenters[dc_id]
+        obs = {
+            'dc_capacity': dc.datacenter_capacity_mw,
+            'current_workload': dc.workload_m.get_current_workload(),
+            'weather': dc.weather_m.get_current_weather(),
+            'total_power_kw': self.low_level_infos[dc_id]['agent_dc'].get('dc_total_power_kW', 0),
+            'ci': dc.ci_m.get_current_ci(),
+        }
+
+        return np.array(list(obs.values()))
+
 
 def main():
     """Main function."""
-    pass
+    env = HARL_HeirarchicalDCRL(DEFAULT_CONFIG)
+    done = False
+    obs, _ = env.reset(seed=0)
+    total_reward = 0
+    
+    greedy_optimizer = WorkloadOptimizer(env.datacenters.keys())
+    
+    max_iterations = 4*24*30
+    # Antonio: Keep in mind that each environment is set to have days_per_episode=30. You can modify this parameter to simulate the whole year
+    with tqdm(total=max_iterations) as pbar:
+        while not done:
+    
+            # Random actions
+            # actions = env.action_space.sample()
+            
+            # Do nothing
+            actions = {'sender': 0, 'receiver': 0, 'workload_to_move': np.array([0.0])}
+
+            # One-step greedy
+            # ci = [obs[dc][-1] for dc in env.datacenters]
+            # actions = {'sender': np.argmax(ci), 'receiver': np.argmin(ci), 'workload_to_move': np.array([1.])}
+            
+            # Multi-step Greedy
+            # actions, _ = greedy_optimizer.compute_adjusted_workload(obs)
+
+            obs, reward, terminated, truncated, info = env.step(actions)
+            done = truncated
+            total_reward += reward
+
+            # Update the progress bar
+            pbar.update(1)
+
+    # After simulation, calculate average metrics for each environment
+    average_metrics = {
+        env_id: {metric: sum(values) / len(values) for metric, values in env_metrics.items()}
+        for env_id, env_metrics in env.metrics.items()
+    }
+
+    # Print average metrics for each environment
+    for env_id, env_metrics in average_metrics.items():
+        print(f"Average Metrics for {env.datacenters[env_id].location}:")
+        for metric, value in env_metrics.items():
+            print(f"\t{metric}: {value:,.2f}")
+        print()  # Blank line for readability
+
+    # Sum metrics across datacenters
+    print("Summed metrics across all DC:")
+    total_metrics = {}
+    for metric in env_metrics:
+        total_metrics[metric] = 0.0
+        for env_id in average_metrics:
+            total_metrics[metric] += average_metrics[env_id][metric]
+
+        print(f'\t{metric}: {total_metrics[metric]:,.2f}')
+
+    print("Total reward = ", total_reward)
 
 
 if __name__ == "__main__":
